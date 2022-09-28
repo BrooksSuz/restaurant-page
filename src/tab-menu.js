b09b4e51b1b6d636d452d0a83bcf3d2d28441481
Brooks Susor <susorbrooks@gmail.com>
2022-09-28 10:46:59 -0400

    Finish tab-menu

--- src/tab-menu.js
+++ src/tab-menu.js
@@ -1,19 +1,37 @@
 export function pageLoadMenu() {
-  const content = document.getElementById('content'); 
-  const divMenu = document.createElement('div');
-  const menuHeader = document.createElement('h1'); 
-  const divBev = document.createElement('div'); 
-  const bevWater = document.createElement('div'); 
-  const divSides = document.createElement('div'); 
-  const sideApple = document.createElement('div'); 
-  const divMainDish = document.createElement('div'); 
-  const mainDishBLT = document.createElement('div'); 
+  const content = document.getElementById('content');
+  const menuHeader = document.createElement('h1');
+  const menuContainer = document.createElement('div');
+  const appHeader = document.createElement('h2');
+  const appNachos = document.createElement('p'); 
+  const mainDishHeader = document.createElement('h2');
+  const mainDishSteak = document.createElement('p'); 
+  const sideDishHeader = document.createElement('h2');
+  const sideDishPotato = document.createElement('p'); 
+  const bevHeader = document.createElement('h2');
+  const bevWater = document.createElement('p'); 
 
-  menuHeader.classList.add('menu-header'); 
-  menuHeader.textContent = 'Menu'; 
+  menuHeader.textContent = 'Menu';
+  appHeader.textContent = 'Appetizers';
+  appNachos.textContent = 'Nachos';
+  mainDishHeader.textContent = 'Main Dishes';
+  mainDishSteak.textContent = 'Steak'; 
+  sideDishHeader.textContent = 'Side Dishes'; 
+  sideDishPotato.textContent = 'Potato'; 
+  bevHeader.textContent = 'Beverages';
+  bevWater.textContent = 'Water'; 
 
-  divMenu.classList.add('div-menu'); 
-  divMenu.appendChild(menuHeader); 
-
-  content.appendChild(divMenu); 
+  menuContainer.classList.add('menu-container');
+  content.classList.add('content');
+  
+  menuContainer.appendChild(appHeader);
+  menuContainer.appendChild(appNachos);
+  menuContainer.appendChild(mainDishHeader);
+  menuContainer.appendChild(mainDishSteak); 
+  menuContainer.appendChild(sideDishHeader);
+  menuContainer.appendChild(sideDishPotato); 
+  menuContainer.appendChild(bevHeader); 
+  menuContainer.appendChild(bevWater); 
+  content.appendChild(menuHeader);
+  content.appendChild(menuContainer);
 }
