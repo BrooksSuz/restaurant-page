218e6d29741ef5f833bb57693b2b0a75773ddd6c
Brooks Susor <susorbrooks@gmail.com>
2022-09-29 09:41:30 -0400

    Finish tab-contact

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { pageLoadMenu } from './tab-menu';
 import { pageLoadContact } from './tab-contact';
 import './style.css'; 
 import './menu.css'; 
+import './contact.css'; 
 
-pageLoadMenu(); 
+pageLoadContact(); 
--- src/tab-contact.js
+++ src/tab-contact.js
@@ -1,3 +1,25 @@
 export function pageLoadContact() {
+  const content = document.getElementById('content'); 
+  const socialHeader = document.createElement('h1'); 
+  const socialContainer = document.createElement('ul'); 
   
+  for (let i = 0; i < 3; i++) {
+    let social = document.createElement('li'); 
+    
+    if (i === 0) {
+      social.textContent = 'Twitter: @poopyscoopy';
+    } else if (i === 1) {
+      social.textContent = 'Instagram: @p00pysc00py'; 
+    } else if (i === 2) {
+      social.textContent = 'Facebook: facebook.com/poopyscoopy'; 
+    } 
+
+    socialContainer.appendChild(social); 
+  }
+
+  socialHeader.textContent = 'Contact Us'; 
+
+  content.classList.add ('content'); 
+  content.appendChild(socialHeader);
+  content.appendChild(socialContainer); 
 }
